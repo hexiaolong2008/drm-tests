e206db62c6f51e29ddad8da2dad4f7910e30d67c
Zach Reizner <zachr@google.com>
2016-04-14 13:21:57 -0700

    bsdrm: make framebuffers from multi-planar gbm_bo
    Before this change, bs_drm_fb_create_fd_gbm would assume a single plane
    when creating a framebuffer. With this change, up to four planes with
    handles, offsets, and strides will be properly put into the framebuffer
    object.
    
    Note that this also fixes a small bug in which memory for only a single
    handle was passed to drmModeAddFB2, instead of an array of 4.
    
    BUG=None
    TEST=stripe still works, nv12_test after the refactor lands
    
    Change-Id: I0143d39bd287f183c798e2fddd5b0c4a1e37e3e1
    Reviewed-on: https://chromium-review.googlesource.com/338963
    Commit-Ready: Zach Reizner <zachr@chromium.org>
    Tested-by: Zach Reizner <zachr@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- bsdrm/src/drm_fb.c
+++ bsdrm/src/drm_fb.c
@@ -20,11 +20,33 @@ static int bs_drm_fb_create_fd_gbm(int fd, struct gbm_bo *bo, uint32_t *fb_id)
 	uint32_t width = gbm_bo_get_width(bo);
 	uint32_t height = gbm_bo_get_height(bo);
 	uint32_t format = gbm_bo_get_format(bo);
-	uint32_t handle = gbm_bo_get_handle(bo).u32;
-	uint32_t stride = gbm_bo_get_stride(bo);
-	uint32_t offset = 0;
+	size_t plane_count = gbm_bo_get_num_planes(bo);
 
-	return drmModeAddFB2(fd, width, height, format, &handle, &stride, &offset, fb_id, 0);
+	if (plane_count == 0 || plane_count > 4) {
+		bs_debug_error("buffer object has invalid number of planes: %zu", plane_count);
+		return -EINVAL;
+	}
+
+	uint32_t handles[4] = { 0 };
+	uint32_t strides[4] = { 0 };
+	uint32_t offsets[4] = { 0 };
+
+	for (size_t plane_index = 0; plane_index < plane_count; plane_index++) {
+		handles[plane_index] = gbm_bo_get_plane_handle(bo, plane_index).u32;
+		if (handles[plane_index] == 0) {
+			bs_debug_error("buffer object has missing plane handle (index %zu)",
+				       plane_index);
+			return -EINVAL;
+		}
+		strides[plane_index] = gbm_bo_get_plane_stride(bo, plane_index);
+		if (strides[plane_index] == 0) {
+			bs_debug_error("buffer object has plane stride 0 (index %zu)", plane_index);
+			return -EINVAL;
+		}
+		offsets[plane_index] = gbm_bo_get_plane_offset(bo, plane_index);
+	}
+
+	return drmModeAddFB2(fd, width, height, format, handles, strides, offsets, fb_id, 0);
 }
 
 uint32_t bs_drm_fb_create_gbm(struct gbm_bo *bo)
